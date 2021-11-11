c7e1dbd2f521cee2ca9ebb6d6238b3e30594a6ae
Mattia Righetti <matt95.righetti@gmail.com>
2021-11-12 00:27:03 +0100

    Fix for messages without text

--- src/main.rs
+++ src/main.rs
@@ -19,23 +19,25 @@ async fn run() {
     let bot = Bot::from_env().auto_send();
 
     teloxide::repl(bot, move |message| async move {
-        if message.update.text().unwrap() == "group_stats" {
-            if let Some(stats) = CHAT_SERVER.get_percent(message.chat_id()) {
-                message.answer(stats.pretty_print()).await?;
+        if let Some(text) = message.update.text() {
+            if text == "group_stats" {
+                if let Some(stats) = CHAT_SERVER.get_percent(message.chat_id()) {
+                    message.answer(stats.pretty_print()).await?;
+                } else {
+                    message.answer("You first have to write some messages...").await?;
+                }
             } else {
-                message.answer("You first have to write some messages...").await?;
-            }
-        } else {
-            match message.update.kind {
-                Common(ref common_msg) => {
-                    if let Some(ref user) = common_msg.from {
-                        if let Some(ref username) = user.username {
-                            println!("user {:?} in chat {:?} just sent a message", username, message.chat_id());
-                            CHAT_SERVER.increment(message.chat_id(), username.clone())
+                match message.update.kind {
+                    Common(ref common_msg) => {
+                        if let Some(ref user) = common_msg.from {
+                            if let Some(ref username) = user.username {
+                                println!("user {:?} in chat {:?} just sent a message", username, message.chat_id());
+                                CHAT_SERVER.increment(message.chat_id(), username.clone())
+                            }
                         }
                     }
+                    _ => {}
                 }
-                _ => {}
             }
         }
         respond(())
